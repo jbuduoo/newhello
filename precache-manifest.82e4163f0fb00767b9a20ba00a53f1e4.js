5cbf9d7f90bf27a670f339ffbab086c83f32e67c
jacky <jacky@gmail.com>
2020-08-31 15:45:01 +0800

    20200831 build

---
+++ precache-manifest.82e4163f0fb00767b9a20ba00a53f1e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ce55f87a6d9eb544d71bf560c1990c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6b78c70f8eb46fcffc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e1698e04be2f4a56d8f",
+    "url": "/static/js/2.dcc99f30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dcc99f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6b78c70f8eb46fcffc",
+    "url": "/static/js/main.9ebee266.chunk.js"
+  },
+  {
+    "revision": "f21fed8868039a0fa044",
+    "url": "/static/js/runtime-main.cace65ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
