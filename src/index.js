ba3830f8c5b6c32de61e1e0dd2aac87bb188462b
jacky <jacky@gmail.com>
2020-08-31 16:52:55 +0800

    20200831

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    hello world
   </React.StrictMode>,
   document.getElementById('root')
 );
